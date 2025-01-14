66a376b5d5f5c4fb35b32d98f90b147c153d7457
Damian Piñones <pinonesdamian@gmail.com>
2025-01-14 20:29:50 -0300

    update script create_mod

--- jokers-of-neon-classic/scripts/create_mod.sh
+++ jokers-of-neon-classic/scripts/create_mod.sh
@@ -9,13 +9,10 @@ inspect_result=$(sozo inspect)
 world_address=$(echo "$inspect_result" | awk '/World/ {getline; getline; print $3}')
 echo "World address: $world_address"
 
-loot_boxes_info_address=$(echo "$inspect_result" | grep "jokers_of_neon_classic-loot_boxes_info" | awk '{print $NF}')
-echo "Loot Boxes Info address: $loot_boxes_info_address"
-
 specials_info_address=$(echo "$inspect_result" | grep "jokers_of_neon_classic-specials_info" | awk '{print $NF}')
 echo "Specials Info address: $specials_info_address"
 
-sozo execute registrator_system register_game_mod -c 11,$loot_boxes_info_address,$specials_info_address --wait --world $world_address
+sozo execute registrator_system register_game_mod -c 11,$specials_info_address --wait --world $world_address
 
 mod_id=$(curl -s -X POST -H "Content-Type: application/json" \
     -d '{"query":"{jokersOfNeonGameModModels(first: 500) {totalCount}}"}' \
