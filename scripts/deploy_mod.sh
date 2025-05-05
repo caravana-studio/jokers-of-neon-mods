bb551dd6db75e7f87ba2959b75d02b98353c17b4
Damian Piñones <pinonesdamian@gmail.com>
2025-05-05 14:08:20 -0300

    Update RPC URLs and enhance manifest files for jokers of neon mods
    - Changed RPC URLs in `dojo_prod.toml` and `dojo_dev.toml` to point to the new endpoint.
    - Updated class hashes and addresses in `manifest_prod.json` and `manifest_dev.json` for consistency.
    - Added new functions and events related to library registration in the manifest files.
    - Introduced new backup manifest file for development purposes.
    - Refactored deployment script to streamline contract deployment process.

--- scripts/deploy_mod.sh
+++ scripts/deploy_mod.sh
@@ -59,7 +59,8 @@ fi
 
 # echo "sozo build && sozo inspect && sozo migrate"
 echo "Deploying contracts..."
-sozo_build_output=$(sozo build && sozo inspect && sozo migrate)
+# sozo_build_output=$(sozo build && sozo inspect && sozo migrate)
+sozo build && sozo inspect && sozo migrate
 
 # echo -e "\n✅ deploy mod finish!"
 
