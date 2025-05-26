8e99b3c05d080cdbe74fd1997ebe1c98fab46f09
Damian Piñones <pinonesdamian@gmail.com>
2025-05-26 19:17:31 -0300

    Fix sed command in replace_env.sh to ensure environment variable replacement works correctly

--- scripts/replace_env.sh
+++ scripts/replace_env.sh
@@ -16,7 +16,7 @@ if [ ! -f "$DOJO_DEV_FILE" ]; then
 fi
 
 # Replace the values using sed
-# sed -i '' "s|default = .*|default = \"$MOD_NAME\"|" "$DOJO_DEV_FILE"
+sed -i '' "s|default = .*|default = \"$MOD_NAME\"|" "$DOJO_DEV_FILE"
 sed -i '' "s|rpc_url = .*|rpc_url = \"$RPC_URL\"|" "$DOJO_DEV_FILE"
 sed -i '' "s|account_address = .*|account_address = \"$ACCOUNT_ADDRESS\"|" "$DOJO_DEV_FILE"
 sed -i '' "s|private_key = .*|private_key = \"$PRIVATE_KEY\"|" "$DOJO_DEV_FILE"
